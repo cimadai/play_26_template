de6d592348f5ee713172c700daca30cd216d3ddf
SHIMADA, Daisuke <dice.k1984@gmail.com>
2018-02-27 16:29:30 +0900

    (~) Remove jQuery dependency.

--- app/framework/assets/js/application.ts
+++ app/framework/assets/js/application.ts
@@ -1,9 +1,6 @@
 const Vue = require("vue").default;
 const TopVue = require("./components/Top.vue").default;
 
-$(function () {
-    console.log("hello");
-    new Vue({
-        render: h => h(TopVue)
-    }).$mount("#top");
-});
+new Vue({
+    render: h => h(TopVue)
+}).$mount("#top");
